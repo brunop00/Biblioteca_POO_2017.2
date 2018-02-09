c08e7d5d19b9abce7346ec31b0b128bd60cd3f0f
Bruno Marques Serrano <brunoprogpunk@gmail.com>
2018-02-09 13:59:45 -0300

    Alterando o nome da classe de leitura de dados.

---
+++ biblioteca_poo/src/biblioteca/LeituraValidacaoDados.java
@@ -0,0 +1,20 @@
+package biblioteca;
+import java.util.Scanner;
+public class LeituraValidacaoDados {
+	private Scanner sc;
+	
+	public LeituraValidacaoDados() {
+		sc = new Scanner(System.in);
+	}
+	
+	public String validaNomeDeUsuario() {
+		
+		
+		
+		
+		
+		return s;
+	}
+	
+	
+}
